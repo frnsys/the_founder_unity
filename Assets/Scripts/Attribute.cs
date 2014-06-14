528d2ac68d1c5c9304d4fe4e3ea187c7b36ebaae
Francis Tseng <f+accounts@frnsys.com>
2014-06-14 04:33:19 -0400

    attribute and modifier system in place

---
+++ Assets/Scripts/Attribute.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+
+public enum ModifierType {add, multiply};
+
+public class AttrModifier {
+    public string name;
+    public float value;
+    public int duration;
+    public ModifierType type;
+
+    public AttrModifier(string name_, float value_, int duration_ = 0, ModifierType type_ = ModifierType.add) {
+        name = name_;
+        value = value_;
+        duration = duration_;
+        type = type_;
+    }
+}
+
+public class AttrModifierCollection : System.Collections.ObjectModel.Collection<AttrModifier> {
+    protected override void InsertItem(int index, AttrModifier mod) {
+
+        // Modifiers that add are calculated first,
+        // so they are inserted at the beginning.
+        if (mod.type == ModifierType.add) {
+            base.InsertItem(0, mod);
+
+        // Multiplier mods are calculated at the end.
+        } else {
+            base.InsertItem(Count, mod);
+        }
+
+        if (mod.duration > 0) {
+            Timer timer = new Timer(mod.duration);
+            timer.Elapsed += delegate { Remove(mod); };
+            timer.Start();
+        }
+    }
+}
+
+public class Attr {
+
+    public string name;
+    public float baseValue = 0;
+    public AttrModifierCollection modifiers = new AttrModifierCollection();
+
+    public float finalValue {
+        get {
+            float _finalValue = baseValue;
+            foreach (AttrModifier mod in modifiers) {
+                if (mod.type == ModifierType.add) {
+                    _finalValue += mod.value;
+                } else {
+                    _finalValue *= mod.value;
+                }
+            }
+            return _finalValue;
+        }
+    }
+
+    public Attr(string name_, float baseValue_) {
+        name = name_;
+        baseValue = baseValue_;
+    }
+}
+
+
