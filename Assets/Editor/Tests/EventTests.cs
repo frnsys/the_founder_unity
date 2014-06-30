b8132011fdb699dd7349d65bd3edf28c89948439
Francis Tseng <f+accounts@frnsys.com>
2014-06-30 03:04:37 -0400

    began implementing event effect system for #9

--- Assets/Editor/Tests/EventTests.cs
+++ Assets/Editor/Tests/EventTests.cs
@@ -7,6 +7,20 @@ using System.Collections.Generic;
 
 namespace UnityTest
 {
+    internal class TestEventListener
+    {
+        public GameEvent gameEvent;
+        public GameEvent.Effect effect;
+
+        public TestEventListener(GameEvent gameEvent) {
+            gameEvent.EventEffect += OnEffect;
+        }
+
+        void OnEffect(GameEvent.Effect e) {
+            effect = e;
+        }
+    }
+
 	[TestFixture]
 	internal class EventTests
 	{
@@ -40,5 +54,23 @@ namespace UnityTest
 
             Assert.AreEqual(happeningEvents[0].name, "Some event");
 		}
+
+        [Test]
+        public void EventEffects()
+        {
+            // Add an effect to the event.
+            GameEvent.Effect effect = new GameEvent.Effect("CASH", amount_: 1000f);
+            gE.effects.Add(effect);
+
+            // Our test listener to listen for and capture the effect.
+            TestEventListener eL = new TestEventListener(gE);
+
+            // Trigger the event's effects.
+            gE.Trigger();
+
+            // Check if the effect was captured.
+            Assert.AreEqual(eL.effect.type, "CASH");
+            Assert.AreEqual(eL.effect.amount, 1000f);
+        }
     }
 }
--- Assets/Scripts/Events/GameEvent.cs
+++ Assets/Scripts/Events/GameEvent.cs
@@ -4,6 +4,30 @@ using System.Collections.Generic;
 using SimpleJSON;
 
 public class GameEvent {
+
+    public struct Effect {
+        public string type;
+        public string subtype;
+        public float amount;
+        public string stat;
+        public int id;
+
+        public Effect(string type_,
+                string subtype_ = null,
+                string stat_ = null,
+                float amount_ = 0f,
+                int id_ = 0)
+        {
+            type = type_;
+            subtype = subtype_;
+            stat = stat_;
+            amount = amount_;
+            id = id_;
+        }
+    }
+
+    // Roll to see what events happen, out of a set of specified
+    // candidate events.
     public static List<GameEvent> Roll(List<GameEvent> candidateEvents) {
         List<GameEvent> theseAreHappening = new List<GameEvent>();
 
@@ -18,19 +42,52 @@ public class GameEvent {
         return theseAreHappening;
     }
 
+
     public string name;
     public Stat probability;
+    public List<Effect> effects = new List<Effect>();
 
     public GameEvent(JSONClass prototype) {
-        name = prototype["name"];
+        // TO DO So ugly. If we move to a more sophisticated
+        // event management system, maybe we can clean this up.
+        JSONArray effects__ = prototype["effects"].AsArray;
+        List<Effect> effects_ = new List<Effect>();
+        foreach (JSONNode effect in effects__) {
+            string type = effect["type"];
+            string subtype = effect["subtype"];
+            string stat = effect["stat"];
+            float amount = effect["amount"].AsFloat;
+            int id = effect["id"].AsInt;
+            effects_.Add(new Effect(type, subtype, stat, amount, id));
+        }
+
+        Initialize(prototype["name"], prototype["probability"].AsFloat, effects_);
     }
     public GameEvent(string name_, float probability_) {
+        Initialize(name_, probability_, new List<Effect>());
+    }
+    public void Initialize(string name_, float probability_, List<Effect> effects_) {
         name = name_;
+        effects = effects_;
 
         // Maximum probability is 1,
         // minimum is 0.
         probability_ = Mathf.Clamp(probability_, 0, 1);
         probability = new Stat("Probability", probability_);
+    }
+
+
+
+    // An event which is broadcast for each event effect.
+    public event System.Action<Effect> EventEffect;
+    public void Trigger() {
+        // If there are subscribers to this event...
+        if (EventEffect != null) {
+            foreach (Effect effect in effects) {
+                // Broadcast the event with the effect.
+                EventEffect(effect);
+            }
+        }
     }
 }
 
