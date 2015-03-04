daf7b274b41a9ccc7c64e01ee7a4d7c3af995262
Francis Tseng <f+accounts@frnsys.com>
2015-03-03 22:33:05 -0500

    added audio pitching script

---
+++ Scripts/pitch_audio.sh
@@ -0,0 +1,12 @@
+# requires sox: brew install sox
+sox 220202__gameaudio__teleport-casual.wav labor2.wav gain -3 pad 0 3 reverb 80
+sox 220202__gameaudio__teleport-casual.wav labor2.wav gain -3 pad 0 3 reverb 80 pitch 500
+sox 220202__gameaudio__teleport-casual.wav labor3.wav gain -3 pad 0 3 reverb 80 pitch 700
+sox 220202__gameaudio__teleport-casual.wav labor4.wav gain -3 pad 0 3 reverb 80 pitch 1000
+sox 220202__gameaudio__teleport-casual.wav labor5.wav gain -3 pad 0 3 reverb 80 pitch 1200
+sox 220202__gameaudio__teleport-casual.wav labor6.wav gain -3 pad 0 3 reverb 80 pitch -200
+sox 220202__gameaudio__teleport-casual.wav labor7.wav gain -3 pad 0 3 reverb 80 pitch -500
+sox 220202__gameaudio__teleport-casual.wav labor8.wav gain -3 pad 0 3 reverb 80 pitch -700
+sox 220202__gameaudio__teleport-casual.wav labor9.wav gain -3 pad 0 3 reverb 80 pitch -1200
+sox 220202__gameaudio__teleport-casual.wav labor9.wav gain -3 pad 0 3 reverb 80 pitch -1200
+sox 220202__gameaudio__teleport-casual.wav labor9.wav gain -3 pad 0 3 reverb 80 pitch -1200
