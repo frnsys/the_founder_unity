3123a50303a16ee5f10a62f11e7cb30033937831
Francis Tseng <f+accounts@frnsys.com>
2015-03-07 09:22:12 -0500

    research ui improvements

--- Assets/Scripts/UI/Research/UIManageResearch.cs
+++ Assets/Scripts/UI/Research/UIManageResearch.cs
@@ -5,17 +5,35 @@ using System.Collections.Generic;
 
 public class UIManageResearch : UIFullScreenPager {
     public GameObject techPrefab;
+    private Company playerCompany;
 
     void OnEnable() {
+        playerCompany = GameManager.Instance.playerCompany;
         LoadTechs();
+
+        Company.ResearchCompleted += ReloadTechs;
+    }
+
+    void OnDisable() {
+        Company.ResearchCompleted -= ReloadTechs;
     }
 
     private void LoadTechs() {
         ClearGrid();
-        foreach (Technology t in Technology.LoadAll().Where(t => t.isAvailable(GameManager.Instance.playerCompany))) {
+        foreach (Technology t in Technology.LoadAll().Where(t => t.isAvailable(playerCompany))) {
             GameObject techItem = NGUITools.AddChild(grid.gameObject, techPrefab);
             techItem.GetComponent<UITechnology>().technology = t;
         }
+        foreach (Technology t in playerCompany.technologies) {
+            GameObject techItem = NGUITools.AddChild(grid.gameObject, techPrefab);
+            UITechnology uit = techItem.GetComponent<UITechnology>();
+            uit.technology = t;
+            uit.researched = true;
+        }
         Adjust();
+    }
+
+    private void ReloadTechs(Technology t) {
+        LoadTechs();
     }
 }
--- Assets/Scripts/UI/Research/UITechnology.cs
+++ Assets/Scripts/UI/Research/UITechnology.cs
@@ -12,21 +12,35 @@ public class UITechnology : UIEffectItem {
             image.mainTexture = technology_.icon;
             cost.text = string.Format("Costs {0} research", technology_.cost);
 
-            // TO DO Show effects?
             RenderEffects(technology_.effects);
             AdjustEffectsHeight();
+        }
+    }
+
+    public bool researched {
+        set {
+            button.isEnabled = !value;
+            if (value) {
+                buttonLabel.text = "Researched";
+            } else {
+                buttonLabel.text = "Research";
+            }
         }
     }
 
     public UILabel label;
     public UILabel cost;
     public UILabel description;
+    public UILabel buttonLabel;
+    public UIButton button;
     public UITexture image;
 
     public void SelectTech() {
         UIManager.Instance.Confirm("Are you sure you want to research this technology?", delegate() {
             if (!GameManager.Instance.playerCompany.BuyTechnology(technology_)) {
                 UIManager.Instance.Alert("You don't have enough research for this technology.");
+            } else {
+                researched = true;
             }
         }, null);
     }
