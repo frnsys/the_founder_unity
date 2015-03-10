27a5fea66ee20e2e15b111fa0c0aef5af3391325
Francis Tseng <f+accounts@frnsys.com>
2015-03-10 19:12:58 -0400

    cost for promo items

--- Assets/Scripts/UI/Communications/UIPromo.cs
+++ Assets/Scripts/UI/Communications/UIPromo.cs
@@ -8,12 +8,14 @@ public class UIPromo : MonoBehaviour {
         set {
             promo_ = value;
             label.text = promo_.name;
-            description.text = promo_.description;
             image.mainTexture = promo_.icon;
+            cost.text = string.Format("{0:C0}", promo_.cost);
+            description.text = promo_.description;
         }
     }
 
     public UILabel label;
+    public UILabel cost;
     public UILabel description;
     public UITexture image;
 
